8ddbb6fe2e2312a1ec1fc507e1431c2e9859f902
geremia <germysettesette@gmail.com>
2024-01-29 16:17:17 +0100

    .gitignore + d01p1

--- d01p1/src/main.rs
+++ d01p1/src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // first argument is the input file
+    let input_filename = env::args().nth(1)
+        .expect("First argument should be path to file"); 
+
+    let input = fs::read_to_string(input_filename)
+        .expect("Should have been able to read the file");
+
+    let sum: usize = input.lines()
+        .map(|line| {
+            let mut first_last_digits = String::new();
+            first_last_digits.push(
+                // first digit
+                line.chars().find(|c| c.is_digit(10)).unwrap()
+            );
+            first_last_digits.push(
+                // last digit
+                line.chars().rev().find(|c| c.is_digit(10)).unwrap()
+            );
+
+            first_last_digits.parse::<usize>().unwrap()
+        })
+        .sum();
+    
+    println!("output: {:?}", sum);
 }
