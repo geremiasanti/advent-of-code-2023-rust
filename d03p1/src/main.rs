c1b65cb6eaaf627d2dc2046625acfea017694206
geremia <germysettesette@gmail.com>
2024-02-25 12:35:51 +0100

    d03p1 - don't know how to continue

--- d03p1/src/main.rs
+++ d03p1/src/main.rs
@@ -8,15 +8,27 @@ fn main() {
         .expect("First argument should be path to file");
     let input = fs::read_to_string(input_filename).expect("Should have been able to read the file");
 
-    let parts_sum: usize = input
+    let line_len = input
         .lines()
-        .zip(input.lines().skip(1))
-        .zip(input.lines().skip(2))
+        .next()
+        .expect("Input should have at least one line")
+        .chars()
+        .count();
+    let mut empty_line = String::from_utf8(vec![b'.'; line_len]).unwrap();
+    empty_line.push('\n');
+    let input_extended = empty_line.clone() + &input + &empty_line;
+    println!("input_extended = \n{}", input_extended);
+
+    let parts_sum: usize = input_extended
+        .lines()
+        .zip(input_extended.lines().skip(1))
+        .zip(input_extended.lines().skip(2))
         .map(|((l0, l1), l2)| {
             let l1_parts_sum = 0;
             dbg!(l0, l1, l2);
 
             let mut current_part_buffer = String::new();
+            let mut near_symbol = false;
             for ((c0, c1), c2) in l0.chars().zip(l1.chars()).zip(l2.chars()) {
                 dbg!(&c0, &c1, &c2);
 
